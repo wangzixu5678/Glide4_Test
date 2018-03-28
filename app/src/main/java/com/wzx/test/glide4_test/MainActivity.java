c48b14cd61b313785c3670f218fe6f4a4d51882d
wzx <445826958@qq.com>
2018-03-28 11:12:01 +0800

    First Commit

--- app/src/main/java/com/wzx/test/glide4_test/MainActivity.java
+++ app/src/main/java/com/wzx/test/glide4_test/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "我是分支", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "我是分支", Toast.LENGTH_SHORT).show();
 
 
     }
